ec98c876f62460dec7162f8dfe158a5221ad0776
Meet Gauswami <154598059+meetgauswami@users.noreply.github.com>
2023-12-23 18:53:16 +0530

    Amazon Clone

---
+++ index.js
@@ -0,0 +1,8 @@
+let Cookies = confirm("This website use cookies to help you have a superior and more admissible browsing experience on the website.");
+
+if(Cookies){
+    console.log("So, You Are Now Get Best Experience On The Website")
+}
+else{
+    console.log("That's Ok, Thanks For Visiting Our Website")
+}
